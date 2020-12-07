84385db329f37bad15551b9ebd0a40b995a3dece
三沐 <huanghaisen@deepexi.com>
2020-12-07 17:50:49 +0800

    测试部署

--- gateway.go
+++ gateway.go
@@ -70,7 +70,7 @@ func (p *Proxy) doProxy(w http.ResponseWriter, r *http.Request) {
 	log.Print("time cost", time.Now().Sub(in).Seconds(), "s")
 }
 func (p *Proxy) getRoute(req *http.Request) string {
-	return "http://10.201.0.142:8889"
+	return HostInfo.GetTarget(req)
 }
 func newReverseProxy(target *url.URL) *httputil.ReverseProxy {
 	director := func(req *http.Request) {
